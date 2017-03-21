b813172c3901ed2b807efa85984f1621b8be6455
Philip Fulgham <philfulgham@gmail.com>
2017-03-21 15:31:08 -0400

    Added Chart.js and AdminHomePage design

--- src/pages/admin-home/admin-home.ts
+++ src/pages/admin-home/admin-home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 /*
   Generated class for the AdminHome page.
@@ -12,11 +13,41 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'admin-home.html'
 })
 export class AdminHomePage {
+    
+    @ViewChild('donutCanvas') donutCanvas;
+    donutChart: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdminHomePage');
+    this.donutChart = new Chart(this.donutCanvas.nativeElement, {
+        type: 'doughnut',
+        options: { legend: { position: 'bottom' }, layout: { padding: 10 }},
+data: {
+labels: ["Virginia", "New York", "Illinois", "South Carolina", "Ohio", "New Jersey"],
+datasets: [{
+    label: "Test",
+data: [750, 1147, 675, 432, 703, 450],
+backgroundColor: [
+    'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                        'rgba(255, 159, 64, 0.8)'
+],
+hoverBackgroundColor: [
+    'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+]
+}]
+}
+        });
   }
 
 }
