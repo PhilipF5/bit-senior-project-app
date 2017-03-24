e51d424dd44162a5549911f5a921bf0679e93e2c
Philip Fulgham <philfulgham@gmail.com>
2017-03-24 12:36:23 -0400

    Added logout button (closes #13) and profile card to nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,57 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController, ModalController, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 import { LoginProvider } from '../providers/login';
+import { LoginPage } from '../pages/login/login';
 
 
 @Component({
-  templateUrl: 'app.html'
+  	templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage = HomePage;
 
-	constructor(platform: Platform, public loginProvider: LoginProvider) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+export class MyApp {
+  	rootPage = HomePage;
+
+	constructor(platform: Platform, public loginProvider: LoginProvider, public alertCtrl: AlertController, public modalCtrl: ModalController, public menuCtrl: MenuController) {
+    	platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			StatusBar.styleDefault();
+			Splashscreen.hide();
+    	});
+  	}
+	
+	showLogoutConfirm() {
+		let confirm = this.alertCtrl.create({
+			title: 'Log out?',
+			message: 'Are you sure you want to log out?',
+			buttons: [
+				{
+					text: 'Cancel',
+					handler: () => {
+						
+					}
+				},
+				{
+					text: 'Log Out',
+					handler: () => {
+						this.menuCtrl.close();
+						this.loginProvider.creds = {
+							apiKey: null,
+							error: null,
+							firstName: null,
+							lastName: null,
+							role: null,
+							username: null
+						};
+						let modal = this.modalCtrl.create(LoginPage, {}, {enableBackdropDismiss: false});
+						modal.present();
+					}
+				}
+			]
+		});
+		confirm.present();
+	}
 }
