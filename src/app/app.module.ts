fadba1af87ca3571e317758cdf49b72b3a30ecdf
Philip Fulgham <philfulgham@gmail.com>
2017-04-17 10:22:19 -0400

    Clean up and comment NgModule
    Organized Module for readability. Also added DataProvider and GfxProvider.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,48 @@
+// Import Angular and Ionic components
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { LoginProvider } from '../providers/login';
-import { AccountProvider } from '../providers/account';
-import { AuctionProvider } from '../providers/auction';
-import { ProfileProvider } from '../providers/profile';
-import { LotProvider } from '../providers/lot';
-import { CurrencyPipe } from '../pipes/currency';
+
+// Import app pages
+import { AccountListPage } from '../pages/account-list/account-list';
 import { AccountViewPage } from '../pages/account-view/account-view';
 import { AuctionListPage } from '../pages/auction-list/auction-list';
 import { AuctionViewPage } from '../pages/auction-view/auction-view';
-import { LotPage } from '../pages/lot/lot';
-import { ProfileViewPage } from '../pages/profile-view/profile-view';
-import { AccountListPage } from '../pages/account-list/account-list';
 import { CreateAccountPage } from '../pages/create-account/create-account';
 import { CreateProfilePage } from '../pages/create-profile/create-profile';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { LotPage } from '../pages/lot/lot';
+import { ProfileViewPage } from '../pages/profile-view/profile-view';
 import { ReportsPage } from '../pages/reports/reports';
 
+// Import providers/services
+import { AccountProvider } from '../providers/account';
+import { AuctionProvider } from '../providers/auction';
+import { DataProvider } from '../providers/data';
+import { GfxProvider } from '../providers/gfx';
+import { LoginProvider } from '../providers/login';
+import { LotProvider } from '../providers/lot';
+import { ProfileProvider } from '../providers/profile';
+
+// Import pipes for value transforms
+import { CurrencyPipe } from '../pipes/currency';
+
+// Bootstrap everything
 @NgModule({
 	declarations: [
-		MyApp,
-		HomePage,
-		LoginPage,
-		CurrencyPipe,
+		AccountListPage,
 		AccountViewPage,
 		AuctionListPage,
 		AuctionViewPage,
-		LotPage,
-		ProfileViewPage,
-		AccountListPage,
 		CreateAccountPage,
 		CreateProfilePage,
+		CurrencyPipe,
+		HomePage,
+		LoginPage,
+		LotPage,
+		MyApp,
+		ProfileViewPage,
 		ReportsPage
 	],
 	imports: [
@@ -40,20 +50,32 @@ import { ReportsPage } from '../pages/reports/reports';
 	],
 	bootstrap: [IonicApp],
 	entryComponents: [
-		MyApp,
-		HomePage,
-		LoginPage,
+		AccountListPage,
 		AccountViewPage,
 		AuctionListPage,
 		AuctionViewPage,
-		LotPage,
-		ProfileViewPage,
-		AccountListPage,
 		CreateAccountPage,
 		CreateProfilePage,
+		HomePage,
+		LoginPage,
+		LotPage,
+		MyApp,
+		ProfileViewPage,
 		ReportsPage
 	],
-	providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, LoginProvider, AccountProvider, AuctionProvider, ProfileProvider, LotProvider]
+	providers: [
+		{
+			provide: ErrorHandler,
+			useClass: IonicErrorHandler
+		}, 
+		AccountProvider,
+		AuctionProvider,
+		DataProvider,
+		GfxProvider,
+		LoginProvider,
+		LotProvider,
+		ProfileProvider
+	]
 })
 
 export class AppModule {}
